f92cc5605e61d21a69b1deb8ff61a02d55952122
kumakuma0319 <whiteprankton@icloud.com>
2021-02-07 21:14:34 +0900

    ユーザー管理機能の修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,9 @@ class User < ApplicationRecord
     validates :nickname
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' }
-    validates :first_name_kana, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' }
-    validates :last_name_kana, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角文字を使用してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角文字を使用してください' }
     validates :birth
-    # validates :password
-    # validates :password_confirmation
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
     validates_format_of :password, with: PASSWORD_REGEX_hankaku, message: 'には半角を使用してください'
   end
