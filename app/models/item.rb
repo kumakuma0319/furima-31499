e40c283999ef09bf2b0d59ede33a4b3a6be26a44
kumakuma0319 <whiteprankton@icloud.com>
2021-02-13 18:27:58 +0900

    バリデーションの追加項目

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   validates :postage_payer_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :preparation_day_id, numericality: { other_than: 1 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image, presence: true
 
 end
