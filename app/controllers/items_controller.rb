04898db995fd54433005ad91b3b2cef1085fb0ee
kumakuma0319 <whiteprankton@icloud.com>
2021-02-19 19:15:18 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :item_for_params, only: [:show , :edit, :update, :move_to_index]
+  before_action :move_to_index, only: [:edit, :update]
   
   def index
     @items = Item.all
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
  
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -42,10 +40,13 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
+  end
+
+  def item_for_params
+    @item = Item.find(params[:id])
   end
 end
 
